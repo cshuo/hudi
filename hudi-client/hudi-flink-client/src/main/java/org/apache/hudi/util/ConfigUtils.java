576e0e57cd21fb070159f04adc1262a1eee21386
cshuo <njucshuo@gmail.com>
2025-03-11 20:51:22 +0800

    fix test

--- hudi-client/hudi-flink-client/src/main/java/org/apache/hudi/util/ConfigUtils.java
+++ hudi-client/hudi-flink-client/src/main/java/org/apache/hudi/util/ConfigUtils.java
@@ -3,10 +3,15 @@ package org.apache.hudi.util;
 import org.apache.hudi.common.config.TypedProperties;
 import org.apache.hudi.config.HoodieWriteConfig;
 
+import org.apache.flink.table.types.logical.RowType;
+
 /**
  * todo add doc
  */
 public class ConfigUtils {
+
+  public static final String NO_PRE_COMBINE = "no_precombine";
+
   /**
    * todo add doc
    * @param props
@@ -14,14 +19,18 @@ public class ConfigUtils {
    */
   public static String getPreCombineField(TypedProperties props) {
     String preCombineField = props.getString("precombine.field", null);
-    if (preCombineField != null) {
+    if (preCombineField != null && !preCombineField.equalsIgnoreCase(NO_PRE_COMBINE)) {
       return preCombineField;
     }
     preCombineField = props.getString("write.precombine.field", null);
-    if (preCombineField != null) {
+    if (preCombineField != null && !preCombineField.equalsIgnoreCase(NO_PRE_COMBINE)) {
       return preCombineField;
     }
     preCombineField = props.getString(HoodieWriteConfig.PRECOMBINE_FIELD_NAME.key(), null);
-    return preCombineField;
+    if (preCombineField != null && !preCombineField.equalsIgnoreCase(NO_PRE_COMBINE)) {
+      return preCombineField;
+    } else {
+      return null;
+    }
   }
 }
